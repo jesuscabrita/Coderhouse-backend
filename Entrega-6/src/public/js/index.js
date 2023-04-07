b972080aa22dc08b3ddf670e4b080dd6f3f6c853
Jesus Cabrita <jesusarnaldo115@gmail.com>
2023-04-07 13:01:01 -0300

    Entrega-6

--- Entrega-6/src/public/js/index.js
+++ Entrega-6/src/public/js/index.js
@@ -1,124 +1,210 @@
 const socket = io();
 
-socket.on('newProduct', (producto) => {
-    const listaProductos = document.getElementById('lista-productos');
-    listaProductos.innerHTML = ''
-    console.log('productonuevo', producto);
+///FUNCIONES DEL CHAT///
 
-    producto.forEach(element => {
-        const nuevoProductoHTML =  `
-    <div style="width: 380px;position: relative;box-shadow: 0 2px 7px #dfdfdf;margin: 10px auto;background: #fafafa;">
-        <div style="
-                position: absolute;
-                left: 0;
-                top: 20px;
-                text-transform: uppercase;
-                font-size: 13px;
-                font-weight: 700;
-                background: red;
-                color: #fff;
-                padding: 3px 10px;">
-            Sale
-        </div>
-        <div style="display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 300px;
-                background: #f0f0f0">
-            <img style="width: 100%;max-height: 100%;" src=${element.thumbnail} alt=${element.title}>
-        </div>
-        <div style="padding: 30px;">
-            <span style="
-                display: block;
-                font-size: 12px;
-                font-weight: 700;
-                text-transform: uppercase;
-                color: #ccc;">
-                ${element.category}
-            </span>
-            <h4><a href="">${element.title}</a></h4>
-            <p>${element.description}</p>
-            <div style="
-                font-weight: 500;
-                display: block;
-                text-transform: uppercase;
-                color: #363636;
-                text-decoration: none;
-                transition: 0.3s;">
-                <div style=" font-size: 18px;color: #fbb72c;font-weight: 600;"><small>$${element.price}</small></div>
-                <div>${element.code}</div>
-                <div>Stock: ${element.stock}</div>
-            </div>
-        </div>
-    </div>
-    `;
-    listaProductos.insertAdjacentHTML('beforeend', nuevoProductoHTML);
+let user;
+let chatBox = document.getElementById("chatBox");
+let chatSubmit = document.getElementById("chatSumbit");
+
+Swal.fire({
+    title: "Ingresa un correo",
+    input: "text",
+    text: "Ingresa el usuario para comunicarte en el chat",
+    inputValidator: (value) => {
+        return !value && "Necesitas escribir un correo de usuario para continuar";
+    },
+    allowOutsideClick: false,
+}).then((result) => {
+    user = result.value;
+    if (user) {
+        Swal.fire({
+            title: "Usuario conectado",
+            text: `Bienvenido ${user}`,
+            icon: "success",
+            position: "top-end",
+            toast: true,
+            showConfirmButton: false,
+            timer: 2000
+        });
+    }
+});
+
+chatBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
+    chatSubmit.addEventListener("click", sendMessage);
+
+function sendMessage() {
+    if (chatBox.value.trim().length > 0) {
+        socket.emit("message", { user: user, message: chatBox.value });
+        chatBox.value = "";
+    }
+}
+
+socket.on("messageLogs", (data) => {
+    let logs = document.getElementById("messageLogs");
+
+    let messages = "";
+    data.forEach((message) => {
+        let userMessage = `<div style="font-weight: 800; font-size: 22px">${message.user}</div>`
+        let messageStyle = (message.user === user) ? 
+        "background-color: #3a573f; color: rgb(201, 190, 190); padding: 6px; border-radius: 8px; font-size: 18px; margin-bottom: 8px; display: flex; justify-content: end; width: 100%; display: flex; align-items: center;" : 
+        "background-color: #97ba9d; color: black; padding: 6px; border-radius: 8px; font-size: 18px; margin-bottom: 8px; display: flex; justify-content: start; width: 100%; display: flex; align-items: center;";
+        messages += `<div style="${messageStyle}">  ${userMessage} : ${message.message}</div>`;
+    });
+
+    logs.innerHTML = messages;
+});
+
+socket.on("userConnected", (data) => {
+    Swal.fire({
+        title: "Usuario conectado",
+        text: `${user} se ha conectado`,
+        icon: "info",
+        position: "top-start",
+        toast: true,
+        showConfirmButton: false,
+        timer: 2000
     });
 });
 
-socket.on('newProduct', (idProducto) => {
-    const listaProductos = document.getElementById('lista-productos');
-    const productos = listaProductos.children;
-
-    for (let i = 0; i < productos.length; i++) {
-    const producto = productos[i];
-
-    if (producto.getAttribute('data-id') == idProducto) {
-        producto.remove();
-        break;
-    }
-    }
+socket.on("userDisconnected", (data) => {
+    Swal.fire({
+        title: `Usuario ${user} desconectado`,
+        text: `${user} se ha desconectado`,
+        icon: "warning",
+        position: "top-start",
+        toast: true,
+        showConfirmButton: false,
+        timer: 2000
+    });
 });
 
-socket.on('newProduct', (productoActualizado)=> {
-    const listaProductos = document.getElementById('lista-productos');
-    const productoAnterior = listaProductos.querySelector(`[data-id="${productoActualizado._id}"]`);
-    if (productoAnterior) {
-    productoAnterior.innerHTML = `
-    <div style="width: 380px;position: relative;box-shadow: 0 2px 7px #dfdfdf;margin: 10px auto;background: #fafafa;">
-        <div style="
-                position: absolute;
-                left: 0;
-                top: 20px;
-                text-transform: uppercase;
-                font-size: 13px;
-                font-weight: 700;
-                background: red;
-                color: #fff;
-                padding: 3px 10px;">
-            Sale
-        </div>
-        <div style="display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 300px;
-                background: #f0f0f0">
-            <img style="width: 100%;max-height: 100%;" src=${productoActualizado.thumbnail} alt="">
-        </div>
-        <div style="padding: 30px;">
-            <span style="
-                display: block;
-                font-size: 12px;
-                font-weight: 700;
-                text-transform: uppercase;
-                color: #ccc;">
-                ${productoActualizado.category}
-            </span>
-            <h4><a href="">${productoActualizado.title}</a></h4>
-            <p>${productoActualizado.description}</p>
-            <div style="
-                font-weight: 500;
-                display: block;
-                text-transform: uppercase;
-                color: #363636;
-                text-decoration: none;
-                transition: 0.3s;">
-                <div style=" font-size: 18px;color: #fbb72c;font-weight: 600;"><small>$${productoActualizado.price}</small></div>
-                <div>${productoActualizado.code}</div>
-                <div>Stock: ${productoActualizado.stock}</div>
-            </div>
-        </div>
-    </div>
-    `;
-    }
-});
+
+///FUNCIONES DE LOS PRODUCTOS///
+
+
+// socket.on('newProduct', (producto) => {
+//     const listaProductos = document.getElementById('lista-productos');
+//     listaProductos.innerHTML = ''
+//     console.log('productonuevo', producto);
+
+//     producto.forEach(element => {
+//         const nuevoProductoHTML =  `
+//     <div style="width: 380px;position: relative;box-shadow: 0 2px 7px #dfdfdf;margin: 10px auto;background: #fafafa;">
+//         <div style="
+//                 position: absolute;
+//                 left: 0;
+//                 top: 20px;
+//                 text-transform: uppercase;
+//                 font-size: 13px;
+//                 font-weight: 700;
+//                 background: red;
+//                 color: #fff;
+//                 padding: 3px 10px;">
+//             Sale
+//         </div>
+//         <div style="display: flex;
+//                 align-items: center;
+//                 justify-content: center;
+//                 height: 300px;
+//                 background: #f0f0f0">
+//             <img style="width: 100%;max-height: 100%;" src=${element.thumbnail} alt=${element.title}>
+//         </div>
+//         <div style="padding: 30px;">
+//             <span style="
+//                 display: block;
+//                 font-size: 12px;
+//                 font-weight: 700;
+//                 text-transform: uppercase;
+//                 color: #ccc;">
+//                 ${element.category}
+//             </span>
+//             <h4><a href="">${element.title}</a></h4>
+//             <p>${element.description}</p>
+//             <div style="
+//                 font-weight: 500;
+//                 display: block;
+//                 text-transform: uppercase;
+//                 color: #363636;
+//                 text-decoration: none;
+//                 transition: 0.3s;">
+//                 <div style=" font-size: 18px;color: #fbb72c;font-weight: 600;"><small>$${element.price}</small></div>
+//                 <div>${element.code}</div>
+//                 <div>Stock: ${element.stock}</div>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+//     listaProductos.insertAdjacentHTML('beforeend', nuevoProductoHTML);
+//     });
+// });
+
+// socket.on('newProduct', (idProducto) => {
+//     const listaProductos = document.getElementById('lista-productos');
+//     const productos = listaProductos.children;
+
+//     for (let i = 0; i < productos.length; i++) {
+//     const producto = productos[i];
+
+//     if (producto.getAttribute('data-id') == idProducto) {
+//         producto.remove();
+//         break;
+//     }
+//     }
+// });
+
+// socket.on('newProduct', (productoActualizado)=> {
+//     const listaProductos = document.getElementById('lista-productos');
+//     const productoAnterior = listaProductos.querySelector(`[data-id="${productoActualizado._id}"]`);
+//     if (productoAnterior) {
+//     productoAnterior.innerHTML = `
+//     <div style="width: 380px;position: relative;box-shadow: 0 2px 7px #dfdfdf;margin: 10px auto;background: #fafafa;">
+//         <div style="
+//                 position: absolute;
+//                 left: 0;
+//                 top: 20px;
+//                 text-transform: uppercase;
+//                 font-size: 13px;
+//                 font-weight: 700;
+//                 background: red;
+//                 color: #fff;
+//                 padding: 3px 10px;">
+//             Sale
+//         </div>
+//         <div style="display: flex;
+//                 align-items: center;
+//                 justify-content: center;
+//                 height: 300px;
+//                 background: #f0f0f0">
+//             <img style="width: 100%;max-height: 100%;" src=${productoActualizado.thumbnail} alt="">
+//         </div>
+//         <div style="padding: 30px;">
+//             <span style="
+//                 display: block;
+//                 font-size: 12px;
+//                 font-weight: 700;
+//                 text-transform: uppercase;
+//                 color: #ccc;">
+//                 ${productoActualizado.category}
+//             </span>
+//             <h4><a href="">${productoActualizado.title}</a></h4>
+//             <p>${productoActualizado.description}</p>
+//             <div style="
+//                 font-weight: 500;
+//                 display: block;
+//                 text-transform: uppercase;
+//                 color: #363636;
+//                 text-decoration: none;
+//                 transition: 0.3s;">
+//                 <div style=" font-size: 18px;color: #fbb72c;font-weight: 600;"><small>$${productoActualizado.price}</small></div>
+//                 <div>${productoActualizado.code}</div>
+//                 <div>Stock: ${productoActualizado.stock}</div>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+//     }
+// });
--- Entrega-6/src/socket.js
+++ Entrega-6/src/socket.js
@@ -2,46 +2,70 @@ import { Server } from "socket.io";
 import { ProductsDataBase } from "./controllers/products.js";
 
 const socket = {};
-const productDataBase = new ProductsDataBase();
+let messages = [];
 
-socket.connect = async(httpServer) => {
+socket.connect = function (httpServer) {
     socket.io = new Server(httpServer);
-
     let { io } = socket;
 
-    io.on("connection",async (socket) => {
-        const productos = await productDataBase.getProducts()
+    io.on("connection", (socket) => {
         console.log(`${socket.id} connected`);
+        socket.broadcast.emit("userConnected", { user: socket.id, message: `${socket.id} se ha conectado`});
 
-        socket.emit('newProduct', productos);
-
-        socket.on('addProduct', (producto) => {
-            productos.push(producto);
-            io.emit('newProduct', productos);
+        socket.on("message", (data) => {
+            messages.push(data);
+            io.emit("messageLogs", messages);
+            socket.broadcast.emit("messageSent", { user: data.user, message: data.message });
         });
 
-        socket.on('eliminarProducto', (productoId) => {
-            const index = productos.findIndex((producto) => producto._id == productoId);
-            if (index !== -1) {
-                productos.splice(index, 1);
-                io.emit('newProduct', productos);
-            }
-            io.emit('newProduct', productos);
-        });
-
-        socket.on('editarProducto', (productoId) => {
-            const index = productos.findIndex((producto) => producto._id == productoId);
-            if (index !== -1) {
-                productos.splice(index, 1);
-                io.emit('newProduct', productos);
-            }
-            io.emit('newProduct', productos);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('Cliente desconectado');
+        socket.on("disconnect", (data) => {
+            console.log(`${socket.id} disconnected`);
+            socket.broadcast.emit("userDisconnected", { user: socket.id, message: `${socket.id} se ha desconectado` });
         });
     });
 };
+
+// const socket = {};
+// const productDataBase = new ProductsDataBase();
+
+// socket.connect = async(httpServer) => {
+//     socket.io = new Server(httpServer);
+
+//     let { io } = socket;
+
+//     io.on("connection",async (socket) => {
+//         const productos = await productDataBase.getProducts()
+//         console.log(`${socket.id} connected`);
+
+//         socket.emit('newProduct', productos);
+
+//         socket.on('addProduct', (producto) => {
+//             productos.push(producto);
+//             io.emit('newProduct', productos);
+//         });
+
+//         socket.on('eliminarProducto', (productoId) => {
+//             const index = productos.findIndex((producto) => producto._id == productoId);
+//             if (index !== -1) {
+//                 productos.splice(index, 1);
+//                 io.emit('newProduct', productos);
+//             }
+//             io.emit('newProduct', productos);
+//         });
+
+//         socket.on('editarProducto', (productoId) => {
+//             const index = productos.findIndex((producto) => producto._id == productoId);
+//             if (index !== -1) {
+//                 productos.splice(index, 1);
+//                 io.emit('newProduct', productos);
+//             }
+//             io.emit('newProduct', productos);
+//         });
+
+//         socket.on('disconnect', () => {
+//             console.log('Cliente desconectado');
+//         });
+//     });
+// };
 
 export default socket;
